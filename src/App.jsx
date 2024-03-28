eb14bf0367d545ace8622376711ab25cd761e0f4
Carlos-Pimentel <157527363+CarlosPimentelA@users.noreply.github.com>
2024-03-28 18:45:28 -0400

    En proceso de crear la pagina del carrito

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import './App.css'
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
-import NavBar from './components/NavBar'
+import "./App.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 function App() {
-
-
   return (
     <BrowserRouter>
-    <NavBar />
-    <Routes>
-     <Route path='/cart'>
-    
-     </Route>
-     </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,28 @@
+import "./Cart.css";
+import CardProductCart from "./CardProductCart.jsx";
 function Cart() {
   return (
     <>
       <section className="cart-section">
-
+        <main className="products-list-cart-main">
+          <div className="cards-list">
+            <CardProductCart />
+            <CardProductCart />
+            <CardProductCart />
+          </div>
+        </main>
+        <aside>
+          <div className="list-buy-cart-div">
+            <ol className="list-buy-cart-list">
+              <li>Product Name |  Product Price</li>
+              <br />
+              <li>Product Name |  Product Price</li>
+              <br />
+              <li>Product Name |  Product Price</li>
+              <br />
+            </ol>
+          </div>
+        </aside>
       </section>
     </>
   );
