451bebf7936ee7ca537c45cdb4947e30aedf32ba
Carlos-Pimentel <157527363+CarlosPimentelA@users.noreply.github.com>
2024-03-28 19:02:59 -0400

    quitar cosas innecesarias

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/cart"></Route>
       </Routes>
     </BrowserRouter>
   );
