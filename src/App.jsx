0acfafd94c5ef23b344eaf0c2747efb3a4d04b86
Carlos-Pimentel <157527363+CarlosPimentelA@users.noreply.github.com>
2024-03-31 13:27:01 -0400

    Creacion de la pagina del carrito y de la pagina de contacto, tambien lograr que tanto el navbar como las paginas mencionadas anteriormente sean responsive.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/cart"></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </BrowserRouter>
   );
