f77f3191aee67d41b62f44bd28297f3cf9fa7bba
Carlos-Pimentel <157527363+CarlosPimentelA@users.noreply.github.com>
2024-04-11 17:28:10 -0400

    conexion a db nube

--- back/dbconnection.js
+++ back/dbconnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectionDb = () => {
   try {
-    mongoose.connect(`mongodb+srv://almontejose:jt99UOxiuG7JJvsM@cluster0.icbz5l7.mongodb.net/`);
+    mongoose.connect(`mongodb+srv://carlos:TyZqeYyHIlMgRC73@cluster0.icbz5l7.mongodb.net/Ecommerce`);
     console.log("Connection succesful");
   } catch (error) {
     console.log("Conection error: ", error.message);
