229e4d0843049507cf193d67c1c7967d41d1f60d
Jose Gabriel <cuevasruizjosegabrile@gmail.com>
2024-04-11 18:09:16 -0400

    Adding login, register and admin AdminPanel
    Arreglando problema con los icons del AdminPanel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import "./App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Login from './components/Login';
+import Register from "./components/Register";
+import AdminPanel from "./components/AdminPanel"
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/cart" ></Route>
-      </Routes>
-    </BrowserRouter>
+ <BrowserRouter>
+   <NavBar />
+   <Routes>
+     <Route path="/cart" ></Route>
+   </Routes>
+   <AdminPanel />
+ </BrowserRouter>
+
+ 
+
+//<BrowserRouter>
+//  <Login />
+//  <Routes>
+//    {/* <Route path="/"/> */}
+//    <Route exact path="/Register" element={<Register />}/>
+//  </Routes>
+//</BrowserRouter>
+ 
+
   );
 }
 
