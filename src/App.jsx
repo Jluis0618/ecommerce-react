334003f4d5c8c1fd7db3e1888b77a4b52a0d1fff
Carlos-Pimentel <157527363+CarlosPimentelA@users.noreply.github.com>
2024-04-11 20:00:20 -0400

    Añadir ruta register

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import Contact from "./pages/Contact";
 import { Products } from "./pages/Products";
 import { HomePage } from "./HomePage";
 import { Footer } from "./components/Footer";
-
+import Register from './components/Register'
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
+        <Route path='/register' element={<Register />}></Route>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/products" element={<Products />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
